c3faf979ee74f1f51b438fe5ec38195ca15b5168
MoingXTwice <sckbs1314@gmail.com>
2022-02-21 18:51:58 +0900

    로그인 기능 구현

--- app.py
+++ app.py
@@ -13,6 +13,22 @@ db = client.petdiary
 def home():
     return render_template('index.html')
 
+# 로그인 api
+@app.route('/login', methods=['POST'])
+def login():
+    id_receive = request.form['user_id']
+    password_receive = request.form['password']
+
+    # password 암호화
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.member.find_one({'user_id' : id_receive, 'password' : password_hash})
+    print(result)
+
+    if result is not None:
+        return jsonify({'result':'success'})
+    else:
+        return jsonify({'fail': '아이디 또는 비밀번호가 일치하지 않습니다.'})
+
 # 회원가입 페이지
 @app.route('/sign_up')
 def sign_up():
@@ -37,7 +53,7 @@ def sign_up_api():
     db.member.insert_one(doc)
     return jsonify({'result':'success'})
 
-# 회원가입시 아이디 중복 체크
+# TODO 회원가입시 아이디 중복 체크
 
 
 if __name__ == '__main__':
